03aef510af897bed72f61ad2bd2bd5757e06452b
Jonathan Timbaldi <7434335+imatimba@users.noreply.github.com>
2026-07-07 19:16:08 -0300

    refactor: separate chirp retrieval and user creation handlers.

--- chirps.go
+++ chirps.go
@@ -8,45 +8,6 @@ import (
 	"github.com/imatimba/http-server/internal/database"
 )
 
-func (cfg *apiConfig) handlerCreateUser(w http.ResponseWriter, r *http.Request) {
-	type parameters struct {
-		Email string `json:"email"`
-	}
-
-	params := parameters{}
-	err := json.NewDecoder(r.Body).Decode(&params)
-
-	if err != nil {
-		http.Error(w, "Invalid request payload", http.StatusBadRequest)
-		return
-	}
-
-	user, err := cfg.db.CreateUser(r.Context(), params.Email)
-
-	if err != nil {
-		http.Error(w, "Failed to create user", http.StatusInternalServerError)
-		return
-	}
-
-	respondWithJSON(w, http.StatusCreated, user)
-}
-
-func (cfg *apiConfig) handlerDeleteAllUsers(w http.ResponseWriter, r *http.Request) {
-	if cfg.platform != "dev" {
-		http.Error(w, "Forbidden, not running in dev mode", http.StatusForbidden)
-		return
-	}
-
-	err := cfg.db.DeleteAllUsers(r.Context())
-	if err != nil {
-		http.Error(w, "Failed to delete users", http.StatusInternalServerError)
-		return
-	}
-	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
-	w.WriteHeader(http.StatusOK)
-	w.Write([]byte("Reset OK"))
-}
-
 func (cfg *apiConfig) handlerCreateChirp(w http.ResponseWriter, r *http.Request) {
 	type parameters struct {
 		Body   string `json:"body"`
@@ -84,4 +45,14 @@ func (cfg *apiConfig) handlerCreateChirp(w http.ResponseWriter, r *http.Request)
 	}
 
 	respondWithJSON(w, http.StatusCreated, chirp)
+}
+
+func (cfg *apiConfig) handlerGetChirps(w http.ResponseWriter, r *http.Request) {
+	chirps, err := cfg.db.GetAllChirps(r.Context())
+	if err != nil {
+		http.Error(w, "Failed to retrieve chirps", http.StatusInternalServerError)
+		return
+	}
+
+	respondWithJSON(w, http.StatusOK, chirps)
 }
--- main.go
+++ main.go
@@ -85,6 +85,7 @@ func main() {
 
 	mux.HandleFunc("POST /api/users", apiCfg.handlerCreateUser)
 	mux.HandleFunc("POST /api/chirps", apiCfg.handlerCreateChirp)
+	mux.HandleFunc("GET /api/chirps", apiCfg.handlerGetChirps)
 
 	log.Printf("Serving files from %s on port: %s\n", filepathRoot, port)
 	log.Fatal(server.ListenAndServe())
